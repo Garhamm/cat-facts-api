3b8e62c9aa1c175731420a59f9af1cb3f1d8a2c8
Garrett Hamman <garrett.hamman@gmail.com>
2022-04-19 13:59:15 -0500

    second commit

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
+
+const br = document.createElement('br')
 function getFetch(){
   // const choice = document.querySelector('input').value
   // console.log(choice)
-  const url = `https://catfact.ninja/fact?max_length=300`
+  const url = `https://v2.jokeapi.dev/joke/Programming`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        // if(data.media_type === "image"){
-        //   document.querySelector('img').src = data.hdurl
-        // }else if(data.media_type === 'video'){
-        //   document.querySelector('iframe').src = data.url
-        // }else{
-        //   alert('Media Not Supported - Contact NASA IMMEDIATLY')
-        // }
-       
-        document.querySelector('h3').innerText = data.fact
+        const br = document.createElement("br")
+        if(data.type === 'twopart'){
+          document.querySelector('h3').innerText = data.setup + data.delivery
+        }else
+        document.querySelector('h3').innerText = data.joke
       })
       .catch(err => {
           console.log(`error ${err}`)
